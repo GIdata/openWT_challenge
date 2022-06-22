600af8195d68267df62013eae069531c54587df5
Gabriel Ineichen <gabriel.ineichen@students.fhnw.ch>
2022-06-22 11:20:51 +0200

    externalized configuration added readme information

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # OpenWT Boat Challenge
 
 Code by Gabriel Ineichen
+
+## Run the code
+
+To start the project, first run the frontend build and afterwards start the backend server,
+which will serve the frontend static files and provide a REST API with wich the frontend communicates.
+
+```
+$> cd frontend
+$> npm install && npm run build
+$> cd ../backend
+$> mvn spring-boot:run
+```
+
+
+## Frontend configuration
+To change the location of the REST API the field `VITE_SERVER_URL` in the .env file can be adjusted.
+After that the client must be recompiled.
+
+## Backend configuration
+The backend server can be configured with the entries in the application.properties file.
+Any spring-boot specific configurations can be changed, for example server address and port.
+
+The application has currently one admin user which can access the API.
+The Username and password can also be specified.
+The Default configuration is username admin and a randomly generated password,
+which is stated at the start of the server.
--- backend/src/main/java/ch/ineichen/openWTChallenge/WebSecurityConfig.java
+++ backend/src/main/java/ch/ineichen/openWTChallenge/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package ch.ineichen.openWTChallenge;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,16 +16,19 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Value("${boatapp.admin-user}")
+    protected String adminUsername;
+    @Value("${boatapp.admin-password}")
+    protected String adminPassword;
+
     @Bean
     public UserDetailsService userDetailsService() {
+        System.out.println("Admin User(" + adminUsername + "): " + adminPassword);
         var passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         var manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("admin")
-                .password("admin")
-                .passwordEncoder(pw -> {
-                    System.out.println(pw);
-                    return passwordEncoder.encode(pw);
-                })
+        manager.createUser(User.withUsername(adminUsername)
+                .password(adminPassword)
+                .passwordEncoder(passwordEncoder::encode)
                 .roles("ADMIN").build());
         return manager;
     }
